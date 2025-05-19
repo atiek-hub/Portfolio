31347a6adf6b15f5ff345338f0194c7004c33532
keita miyajima <k.msli2218@gmail.com>
2025-05-19 15:04:23 +0900

    fix:next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/docs",
   images: { unoptimized: true },
-  distDir: "./docs", // ビルド出力をdocsにする（GitHub Pages対応）
+  distDir: "/docs", // ビルド出力をdocsにする（GitHub Pages対応）
 };
 
 export default nextConfig;
